a87afe5a6998f8f622c4555926ebf6af7f7129b6
bjborchert93 <bjborchert93@gmail.com>
2022-11-02 18:51:41 -0500

    updated with CRUD functionality

--- server.py
+++ server.py
@@ -12,24 +12,42 @@ def get_all_users():
     # call the get all classmethod to get all friends
     users = User.get_all()
     print(users)
-    return render_template("read.html", users=users)
+    return render_template("read_all.html", users=users)
 
-@app.route('/new_user')
+@app.route('/user/new')
 def new_user():
     return render_template("create.html")
 
-@app.route('/create_user', methods=['POST'])
+@app.route('/user/create', methods=['POST'])
 def create_user():
-
     data = {
-        "fname": request.form["fname"],
-        "lname": request.form["lname"],
+        "first_name": request.form["first_name"],
+        "last_name": request.form["last_name"],
         "email": request.form["email"]
     }
-
     User.save(data)
-
     return redirect('/user')
-            
+
+@app.route('/user/edit/<int:id>')
+def edit(id):
+    data = {"id": id}
+    return render_template("edit.html", user=User.get_user(data))
+
+@app.route('/user/show/<int:id>')
+def show(id):
+    data = {"id": id}
+    return render_template("read_one.html", user=User.get_user(data))
+
+@app.route('/user/update', methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/user')
+
+@app.route('/user/destroy/<int:id>')
+def destroy(id):
+    data = {'id': id}
+    User.destroy(data)
+    return redirect('/user')
+
 if __name__ == "__main__":
     app.run(debug=True)
